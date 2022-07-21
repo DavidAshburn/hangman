a66701495a628d95e140b6656b32bb75b1941707
DavidAshburn <oneinchwebbing@gmail.com>
2022-07-21 15:22:07 -0700

    file saving and loading complete

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,44 +1,108 @@
+require 'yaml'
+
 dict_path = File.join(File.dirname(__FILE__), '../assets/dictionary.txt')
 $file_in = File.read(dict_path).split("\n")
 
 $pics_path = File.join(File.dirname(__FILE__), '../assets/hang_7height.txt')
 $pics_in = File.read($pics_path).split("\n")
 
-module ReadASCII
-  def ascii_slice(_file_path, frame_height)
-    
-    frame_count = $pics_in.length / frame_height
-    @frames = []
+$save_path = File.join(File.dirname(__FILE__), '../save/save_file.yaml')
+if(File.file?($save_path))
+  $save = YAML.load(File.read($save_path))
+end
 
-    for i in 0...frame_count
-      @frames.push($pics_in[0...frame_height])
-      for i in 0...frame_count
-        $pics_in.shift
+module BasicSerializable
+  @@serializer = YAML
+
+  def serialize
+    obj = {}
+    instance_variables.map do |var|
+      if var != :@dictionary
+        obj[var] = instance_variable_get(var)
       end
+    end
+    @@serializer.dump obj
+  end
+
+  def unserialize(string)
+    $save.keys.each do |key|
+      instance_variable_set(key, $save[key])
     end
   end
 end
 
+
 class Hangman
-  include ReadASCII
-  def initialize
+include BasicSerializable
+
+  attr_accessor :frames, :bad_guesses, :chosen_bads, :secret_word, :working_word
+
+  def initialize(frames = [], bad_guesses = 0, chosen_bads = [], secret_word = [], working_word = [])
     @dictionary = $file_in.each_with_object([]) do |value, list|
       list.push(value) if value.length.between?(5, 12)
     end
+    @frames = frames
+    @bad_guesses = bad_guesses
+    @chosen_bads = chosen_bads
+    @secret_word = secret_word
+    @working_word = working_word
+    ascii_slice(7)
+  end
 
-    ascii_slice($pics_path, 7)
+  #---  Game Controller Methods  ---#
+  public
+  def put_title
+    puts "\n  /\\  /\\  __ _  _ __    __ _          /\\/\\    __ _  _ __  "
+    puts " / /_/ / / _` || '_ \\  / _` | _____  /    \\  / _` || '_ \\ "
+    puts '/ __  / | (_| || | | || (_| ||_____|/ /\\/\\ \\| (_| || | | |'
+    puts '\\/ /_/   \\__,_||_| |_| \\__, |       \\/    \\/ \\__,_||_| |_|'
+    puts '                       |___/                              '
+  end
 
-    put_title
+  def save_notice
+    puts "You can save your game anytime by entering the tilde '~' character as your guess\n"
+  end
 
-    new_game
+  def load_check
+    if File.file?("save/save_file.yaml")
+      puts "Would you like to load your last game? y/n"
+      input = gets.chomp.downcase
+      if(input[0] == 'y')
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
+
+  def load_game
+    puts @frames[@bad_guesses]
+    puts "Tried : #{@chosen_bads.join(" ")}\n"
+    puts "Correct: #{@working_word.join(" ")}\n"
+    new_guess
+    while @bad_guesses < 6 do
+      new_guess
+      if @working_word == @secret_word
+        win_game
+        win = true
+      end      
+    end
+    if !win 
+      lose_game
+    end
+    ask_new_game
   end
 
   def new_game
     @bad_guesses = 0
+    @chosen_bads = []
 
     new_words
     puts @frames[0]
-    puts @working_word.join(" ")
+    puts "Tried : #{@chosen_bads.join(" ")}\n"
+    puts "Correct: #{@working_word.join(" ")}\n"
 
     while @bad_guesses < 6 do
       new_guess
@@ -53,7 +117,10 @@ class Hangman
     ask_new_game
   end
 
+  #---  Internal Methods  ---#
+  private
   def win_game
+    File.delete(@save_path) if File.exist?(@save_path)
     puts "You won!"
     puts "#{@working_word.join} was correct!"
     puts "Start a new game? y/n"
@@ -68,6 +135,7 @@ class Hangman
   end
 
   def lose_game
+    File.delete(@save_path) if File.exist?(@save_path)
     puts "Sorry you were hung!"
     puts "The secret word was #{@secret_word.join}"
     puts "Start a new game? y/n"
@@ -86,6 +154,13 @@ class Hangman
     input = gets.chomp.downcase
     correct = false
 
+    if input == '~'
+      $save = File.open($save_path, "w")
+      $save.write(self.serialize)
+      puts "Game saved"
+      exit(0)
+    end
+
     @secret_word.each_with_index do |letter,dex|
       if input == letter
         @working_word[dex] = input
@@ -94,9 +169,11 @@ class Hangman
     end
     if !correct
       @bad_guesses += 1
+      @chosen_bads.push(input)
     end
     puts @frames[@bad_guesses]
-    puts @working_word.join(" ")
+    puts "Tried : #{@chosen_bads.join(" ")}\n"
+    puts "Correct: #{@working_word.join(" ")}\n"
   end
 
   def new_words
@@ -107,13 +184,31 @@ class Hangman
     end
   end
   
-  def put_title
-    puts "\n  /\\  /\\  __ _  _ __    __ _          /\\/\\    __ _  _ __  "
-    puts " / /_/ / / _` || '_ \\  / _` | _____  /    \\  / _` || '_ \\ "
-    puts '/ __  / | (_| || | | || (_| ||_____|/ /\\/\\ \\| (_| || | | |'
-    puts '\\/ /_/   \\__,_||_| |_| \\__, |       \\/    \\/ \\__,_||_| |_|'
-    puts '                       |___/                              '
+  def ascii_slice(frame_height)
+    
+    frame_count = $pics_in.length / frame_height
+  
+    for i in 0...frame_count
+      @frames.push($pics_in[0...frame_height])
+      for i in 0...frame_count
+        $pics_in.shift
+      end
+    end
   end
 end
+#--------------------------------------------------------------------->end Hangman
 
-dic = Hangman.new
+
+#--- Controller ---#
+game = Hangman.new
+game.put_title
+game.save_notice
+
+if game.load_check
+  game.unserialize($save)
+  game.load_game
+else
+  game.new_game
+end
+
+
